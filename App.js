eeeafa65b734ddac7b7e9d9dbefe3dce348ab660
EchoEkhi <echoekhi@gmail.com>
2025-05-16 06:00:29 +0100

    Added animation

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Button, ScrollView, Animated, View } from 'react-native';
 import { Picker } from '@expo/ui/jetpack-compose';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function App() {
   const [show, setShow] = useState(true)
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: show ? 1 : 0.1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [show]);
 
   return (
     <ScrollView style={styles.container}>
       <StatusBar style="auto" />
       <Button
-        title='Press this button and immediately try to scroll to see the lag'
+        title='Press this button to see the lag'
         onPress={() => setShow(!show)}
       />
       <View
         importantForAccessibility={show ? "auto" : "no-hide-descendants"}
       >
-        {Array.from({ length: 30 }).map((_, idx) => (
-          <Picker
-            key={idx}
-            options={['$', '$$', '$$$', '$$$$']}
-          />
-        ))}
+        <Animated.View style={{ opacity }}>
+          {Array.from({ length: 50 }).map((_, idx) => (
+            <Picker
+              key={idx}
+              options={['$', '$$', '$$$', '$$$$']}
+            />
+          ))}
+        </Animated.View>
       </View>
     </ScrollView>
   );
